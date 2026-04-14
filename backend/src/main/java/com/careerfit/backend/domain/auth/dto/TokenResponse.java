dc70b57a696bed8f424faab3955dc066e6a44e85
changtaeyoung <mickey470379@gmail.com>
2026-04-14 14:39:54 +0900

    feat: TokenResponse 생성

---
+++ backend/src/main/java/com/careerfit/backend/domain/auth/dto/TokenResponse.java
@@ -0,0 +1,16 @@
+package com.careerfit.backend.domain.auth.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResponse {
+    
+    private String accessToken;
+    private String refreshToken;
+
+    @Builder.Default
+    private String tokenType = "Bearer";
+
+}
