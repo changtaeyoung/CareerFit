4260ef3fd07140c5c58a5c2a4e7b2a6508265417
changtaeyoung <mickey470379@gmail.com>
2026-04-14 14:42:24 +0900

    feat: ValidException 처리 기능 추가

--- backend/src/main/java/com/careerfit/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/careerfit/backend/common/exception/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.careerfit.backend.common.exception;
 
 import com.careerfit.backend.common.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,6 +27,27 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(
                 ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR),
                 ErrorCode.INTERNAL_SERVER_ERROR.getStatus()
+        );
+    }
+
+    // DTO에 적어준 메시지를 그대로 반환시켜주기 위함
+    // @Valid를 통해 DTO에 적은 메시지 반환시켜준다는 것을 의미함
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidException(
+            MethodArgumentNotValidException e) {
+
+        // 여러 필드 에러 중 첫 번째 메시지만 꺼냄
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+
+        log.error("[Validation] message: {}", message);
+
+        return new ResponseEntity<>(
+                ApiResponse.fail(ErrorCode.INVALID_INPUT),
+                HttpStatus.BAD_REQUEST
         );
     }
 }
